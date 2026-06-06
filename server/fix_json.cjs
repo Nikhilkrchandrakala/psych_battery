a3b960dbb5b7367860443da13743e1075b549ece
Quantum Climb <qc@quantum-climb.com>
2026-06-06 19:34:39 +0530

    chore: move all DB utility scripts to scripts/DBQUERY and gitignore them

--- server/fix_json.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-['male_battery.json', 'female_battery.json'].forEach(file => {
-  let content = fs.readFileSync(file, 'utf8');
-  content = content.replace(/"slideType": "INSTRUCTIONS"/g, '"slideType": "TEXT"');
-  content = content.replace(/"slideType": "SITUATION"/g, '"slideType": "TEXT"');
-  fs.writeFileSync(file, content);
-  console.log(file, 'updated');
-});
